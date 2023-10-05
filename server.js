1187c341992f13cd1001041b5926d1ccf45a9c1e
Abigail <cinco.de.abby@gmail.com>
2023-10-05 16:08:55 -0400

    Added api routes and worked out errors

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express =  require('express');
 const app = express();
-const path = require('path');
 const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoute');
+const htmlRoutes = require('./routes/htmlRoute');
 
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+
+app.use('/api', apiRoutes);
+app.use(htmlRoutes);
 
 app.use(express.static('public'));
 
